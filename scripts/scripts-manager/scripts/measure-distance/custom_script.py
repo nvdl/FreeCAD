6f664e504a42749900d32989c0fabc9031d72f9b
Naveed Alam <naveedguy@gmail.com>
2024-12-27 05:00:50 +0100

    Add license text and cleanup imports

--- scripts/scripts-manager/scripts/measure-distance/custom_script.py
+++ scripts/scripts-manager/scripts/measure-distance/custom_script.py
@@ -1,5 +1,29 @@
+'''
+***************************************************************************
+*                                                                         *
+*   Author:  Naveed Alam.                                                 *
+*   Email:   naveedguy ayt gmail dot com                                  *
+*   Source:  https://github.com/nvdl/FreeCAD                              *
+*   License: https://github.com/FreeCAD/FreeCAD/blob/main/LICENSE         *
+*                                                                         *
+***************************************************************************
+*                                                                         *
+*   This file is a supplement to the FreeCAD CAx development system.      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+*   as published by the Free Software Foundation; either version 2 of     *
+*   the License, or (at your option) any later version.                   *
+*   for detail see the LICENCE text file.                                 *
+*                                                                         *
+*   This software is distributed in the hope that it will be useful,      *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+*   See the GNU Library General Public License for more details.          *
+*                                                                         *
+***************************************************************************
+'''
 import FreeCAD
-import FreeCADGui
 import Part
 # ==============================================================================
 class CustomScript():
--- scripts/scripts-manager/scripts/set-group-from-label-selection/custom_script.py
+++ scripts/scripts-manager/scripts/set-group-from-label-selection/custom_script.py
@@ -1,3 +1,28 @@
+'''
+***************************************************************************
+*                                                                         *
+*   Author:  Naveed Alam.                                                 *
+*   Email:   naveedguy ayt gmail dot com                                  *
+*   Source:  https://github.com/nvdl/FreeCAD                              *
+*   License: https://github.com/FreeCAD/FreeCAD/blob/main/LICENSE         *
+*                                                                         *
+***************************************************************************
+*                                                                         *
+*   This file is a supplement to the FreeCAD CAx development system.      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+*   as published by the Free Software Foundation; either version 2 of     *
+*   the License, or (at your option) any later version.                   *
+*   for detail see the LICENCE text file.                                 *
+*                                                                         *
+*   This software is distributed in the hope that it will be useful,      *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+*   See the GNU Library General Public License for more details.          *
+*                                                                         *
+***************************************************************************
+'''
 import FreeCAD
 # ==============================================================================
 class CustomScript():
@@ -48,7 +73,7 @@ class CustomScript():
 # ==============================================================================
     def about(self) -> str:
 
-        aboutStr = ("Set group label of objects using the saved label of selection.\n"
+        aboutStr = ("Move objects to a group (set group label of objects) using the saved label of selection.\n"
                     "Use the selection script to choose the label of the target group.")
 
         return aboutStr
