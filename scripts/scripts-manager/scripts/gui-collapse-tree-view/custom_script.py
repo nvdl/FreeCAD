637a267d38277bf57414e113f4da590c0ca40ec0
Naveed Alam <naveedguy@gmail.com>
2025-04-18 18:57:47 +0200

    Add a script to collapse the tree view

---
+++ scripts/scripts-manager/scripts/gui-collapse-tree-view/custom_script.py
@@ -0,0 +1,53 @@
+'''
+***************************************************************************
+*                                                                         *
+*   Author:  Naveed Alam                                                  *
+*   Email:   naveedguy ayt gmail dot com                                  *
+*   Source:  https://github.com/nvdl/FreeCAD                              *
+*   License: https://github.com/FreeCAD/FreeCAD/blob/main/LICENSE         *
+*                                                                         *
+***************************************************************************
+*                                                                         *
+*   This file is a supplement to the FreeCAD CAx development system.      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+*   as published by the Free Software Foundation; either version 2 of     *
+*   the License, or (at your option) any later version.                   *
+*   for detail see the LICENCE text file.                                 *
+*                                                                         *
+*   This software is distributed in the hope that it will be useful,      *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+*   See the GNU Library General Public License for more details.          *
+*                                                                         *
+***************************************************************************
+'''
+from PySide.QtWidgets import QTreeWidget
+
+import FreeCADGui
+# ==============================================================================
+class CustomScript():
+
+    def __init__(self, parent, modulePath) -> None:
+
+        self.parent = parent
+        self.modulePath = modulePath
+        self.common = self.parent.getModule(modulePath="common", name="common", relative=False)
+# ==============================================================================
+    def run(self) -> None:
+
+        self.parent.statusMessage(f"Running \"{self.modulePath}\"")
+
+        mw = FreeCADGui.getMainWindow()
+        trees = mw.findChildren(QTreeWidget)
+        trees[0].collapseAll()
+
+        self.parent.statusMessage(f"Done \"{self.modulePath}\"")
+# ==============================================================================
+    def about(self) -> str:
+
+        aboutStr = "Collapse the tree view of objects in the GUI."
+
+        return aboutStr
+# ==============================================================================
