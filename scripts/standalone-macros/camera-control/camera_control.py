bb4303ff566c9e0f90296a7b965de0a98adc2afb
Naveed Alam <naveedguy@gmail.com>
2026-09-26 00:32:11 +0200

    Add support for XY plane based navigation

--- scripts/standalone-macros/camera-control/camera_control.py
+++ scripts/standalone-macros/camera-control/camera_control.py
@@ -34,19 +34,27 @@
 # ==================================================================================================
 import math
 
-from PySide.QtCore import *
-from PySide.QtGui import *
-
 import FreeCAD
 import FreeCADGui
-from pivy.coin import SbVec3f
+from PySide.QtCore import QCoreApplication, QEvent, QMetaObject, QRect, QSize, Qt
+from PySide.QtGui import QAction, QKeyEvent
 
+from PySide.QtWidgets import (
+    QCheckBox,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QSizePolicy,
+    QSlider,
+    QTabWidget,
+    QWidget,
+)
 # ==================================================================================================
 __title__ = "CameraControl"
-__version__ = "1.0"
-__date__ = "24/09/2026"
+__version__ = "2.0"
+__date__ = "26/09/2026"
 __author__ = "Naveed Alam"
-__Requires__ = "Freecad 1.0.0"
+__Requires__ = "Freecad 1.1.0"
 __Status__ = "stable"
 __Comment__ = "This macro allows for precise positioning of the camera using the keyboard."
 __url__ = "http://www.freecadweb.org/"
@@ -115,8 +123,18 @@ class MacroWindow(QMainWindow):
         self.ui.btnDown.clicked.connect(self.btnDownClicked)
 
         # styleSheet = "QPushButton {font-size: 12px; min-width: 30px; max-width: 30px;}"
-        styleSheet = "QPushButton {min-width: 30px; max-width: 30px;}"
-        self.setStyleSheet(styleSheet)
+        # styleSheet = "QPushButton {min-width: 30px; max-width: 30px;}"
+        # self.setStyleSheet(styleSheet)
+
+        self.setStyleSheet("""
+            QPushButton {
+                min-width: 30px;
+                max-width: 30px;
+                min-height: 30px;
+                max-height: 30px;
+                padding: 0px 0px;
+            }
+        """)
 
         # Synchronize the labels.
         self.sldTranslateDeltaChanged()
@@ -201,10 +219,18 @@ class MacroWindow(QMainWindow):
 
         # Convert orientation quaternion to "FreeCAD.Rotation".
         qTuple = camera.orientation.getValue().getValue()
-        cameraRotation = FreeCAD.Rotation(*qTuple)
+        cameraDir = FreeCAD.Rotation(*qTuple)
+
+        if self.ui.chkXYRestricted.isChecked():
+            # FreeCAD uses Yaw (around Z), Pitch (around X), Roll (around Y).
+            # Angles are in degrees.
+            yaw, pitch, roll = cameraDir.getYawPitchRoll()
+
+            roll = 90
+            cameraDir = FreeCAD.Rotation(yaw, pitch, roll)
 
         # Transform camera space to world space and normalize.
-        worldDir = cameraRotation.multVec(localDir).normalize()
+        worldDir = cameraDir.multVec(localDir).normalize()
 
         # Invert vector direction for opposite movements.
         if direction in ("backward", "right", "up"):
@@ -309,7 +335,7 @@ class MacroWindow(QMainWindow):
         self.ui.lblInfo.setText(f"X: {cameraPositionX}\n"
                                 f"Y: {cameraPositionY}\n"
                                 f"Z: {cameraPositionZ}\n"
-                                f"OAngle: {cameraOrientationAngle}\n"
+                                f"OAngle: {cameraOrientationAngle} deg\n"
                                 f"OAxis: {round(cameraOrientation.Axis.x, 2)}, "
                                 f"{round(cameraOrientation.Axis.y, 2)}, "
                                 f"{round(cameraOrientation.Axis.z, 2)}")
@@ -318,14 +344,14 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(160, 360)
+        MainWindow.resize(150, 540)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QSize(160, 360))
-        MainWindow.setMaximumSize(QSize(160, 360))
+        MainWindow.setMinimumSize(QSize(150, 450))
+        MainWindow.setMaximumSize(QSize(150, 600))
         self.actionOpen = QAction(MainWindow)
         self.actionOpen.setObjectName(u"actionOpen")
         self.actionClose = QAction(MainWindow)
@@ -337,7 +363,7 @@ class Ui_MainWindow(object):
         self.tabMain = QTabWidget(self.centralwidget)
         self.tabMain.setObjectName(u"tabMain")
         self.tabMain.setEnabled(True)
-        self.tabMain.setGeometry(QRect(0, 0, 671, 461))
+        self.tabMain.setGeometry(QRect(0, 0, 150, 541))
         self.tabMain.setIconSize(QSize(16, 16))
         self.tabMain.setElideMode(Qt.TextElideMode.ElideNone)
         self.tabMain.setDocumentMode(True)
@@ -346,10 +372,10 @@ class Ui_MainWindow(object):
         self.btnUp = QPushButton(self.tab)
         self.btnUp.setObjectName(u"btnUp")
         self.btnUp.setEnabled(True)
-        self.btnUp.setGeometry(QRect(110, 140, 40, 40))
+        self.btnUp.setGeometry(QRect(110, 160, 30, 30))
         self.sldTranslateDelta = QSlider(self.tab)
         self.sldTranslateDelta.setObjectName(u"sldTranslateDelta")
-        self.sldTranslateDelta.setGeometry(QRect(10, 30, 140, 40))
+        self.sldTranslateDelta.setGeometry(QRect(10, 30, 180, 40))
         self.sldTranslateDelta.setMinimum(-9)
         self.sldTranslateDelta.setMaximum(9)
         self.sldTranslateDelta.setPageStep(1)
@@ -360,26 +386,26 @@ class Ui_MainWindow(object):
         self.btnDown = QPushButton(self.tab)
         self.btnDown.setObjectName(u"btnDown")
         self.btnDown.setEnabled(True)
-        self.btnDown.setGeometry(QRect(10, 140, 40, 40))
+        self.btnDown.setGeometry(QRect(10, 160, 30, 30))
         self.btnLeft = QPushButton(self.tab)
         self.btnLeft.setObjectName(u"btnLeft")
         self.btnLeft.setEnabled(True)
-        self.btnLeft.setGeometry(QRect(10, 190, 40, 40))
+        self.btnLeft.setGeometry(QRect(10, 200, 30, 30))
         self.btnForward = QPushButton(self.tab)
         self.btnForward.setObjectName(u"btnForward")
         self.btnForward.setEnabled(True)
-        self.btnForward.setGeometry(QRect(60, 140, 40, 40))
+        self.btnForward.setGeometry(QRect(60, 160, 30, 30))
         self.btnRight = QPushButton(self.tab)
         self.btnRight.setObjectName(u"btnRight")
         self.btnRight.setEnabled(True)
-        self.btnRight.setGeometry(QRect(110, 190, 40, 40))
+        self.btnRight.setGeometry(QRect(110, 200, 30, 30))
         self.btnBackward = QPushButton(self.tab)
         self.btnBackward.setObjectName(u"btnBackward")
         self.btnBackward.setEnabled(True)
-        self.btnBackward.setGeometry(QRect(60, 190, 40, 40))
+        self.btnBackward.setGeometry(QRect(60, 200, 30, 30))
         self.sldHeightAngle = QSlider(self.tab)
         self.sldHeightAngle.setObjectName(u"sldHeightAngle")
-        self.sldHeightAngle.setGeometry(QRect(10, 90, 140, 40))
+        self.sldHeightAngle.setGeometry(QRect(10, 90, 180, 40))
         self.sldHeightAngle.setMinimum(5)
         self.sldHeightAngle.setMaximum(170)
         self.sldHeightAngle.setPageStep(1)
@@ -390,8 +416,43 @@ class Ui_MainWindow(object):
         self.lblHeightAngle.setGeometry(QRect(10, 70, 140, 20))
         self.lblInfo = QLabel(self.tab)
         self.lblInfo.setObjectName(u"lblInfo")
-        self.lblInfo.setGeometry(QRect(10, 240, 140, 80))
+        self.lblInfo.setGeometry(QRect(10, 420, 140, 81))
+        self.lblInfo.setAlignment(Qt.AlignmentFlag.AlignLeading |
+                                  Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        self.chkXYRestricted = QCheckBox(self.tab)
+        self.chkXYRestricted.setObjectName(u"chkXYRestricted")
+        self.chkXYRestricted.setGeometry(QRect(10, 380, 191, 24))
+        self.btnRotLeft = QPushButton(self.tab)
+        self.btnRotLeft.setObjectName(u"btnRotLeft")
+        self.btnRotLeft.setEnabled(True)
+        self.btnRotLeft.setGeometry(QRect(10, 290, 30, 30))
+        self.btnRotUp = QPushButton(self.tab)
+        self.btnRotUp.setObjectName(u"btnRotUp")
+        self.btnRotUp.setEnabled(True)
+        self.btnRotUp.setGeometry(QRect(60, 290, 30, 30))
+        self.btnRotRight = QPushButton(self.tab)
+        self.btnRotRight.setObjectName(u"btnRotRight")
+        self.btnRotRight.setEnabled(True)
+        self.btnRotRight.setGeometry(QRect(110, 290, 30, 30))
+        self.btnRotDown = QPushButton(self.tab)
+        self.btnRotDown.setObjectName(u"btnRotDown")
+        self.btnRotDown.setEnabled(True)
+        self.btnRotDown.setGeometry(QRect(60, 330, 30, 30))
+        self.lbl2 = QLabel(self.tab)
+        self.lbl2.setObjectName(u"lbl2")
+        self.lbl2.setGeometry(QRect(10, 260, 140, 20))
+        self.lbl1 = QLabel(self.tab)
+        self.lbl1.setObjectName(u"lbl1")
+        self.lbl1.setGeometry(QRect(10, 130, 140, 20))
         self.tabMain.addTab(self.tab, "")
+        self.tabHelp = QWidget()
+        self.tabHelp.setObjectName(u"tabHelp")
+        self.lbl3 = QLabel(self.tabHelp)
+        self.lbl3.setObjectName(u"lbl3")
+        self.lbl3.setGeometry(QRect(10, 10, 181, 391))
+        self.lbl3.setAlignment(Qt.AlignmentFlag.AlignLeading |
+                               Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        self.tabMain.addTab(self.tabHelp, "")
         MainWindow.setCentralWidget(self.centralwidget)
         QWidget.setTabOrder(self.sldTranslateDelta, self.btnUp)
         QWidget.setTabOrder(self.btnUp, self.btnForward)
@@ -410,17 +471,39 @@ class Ui_MainWindow(object):
         self.actionClose.setText(QCoreApplication.translate("MainWindow", u"Close", None))
         self.actionAbout.setText(QCoreApplication.translate("MainWindow", u"About", None))
         self.btnUp.setText(QCoreApplication.translate("MainWindow", u"UP", None))
-        self.lblTranslateDelta.setText(QCoreApplication.translate(
-            "MainWindow", u"Step: 1234567890", None))
+        self.lblTranslateDelta.setText(QCoreApplication.translate("MainWindow", u"Step:", None))
         self.btnDown.setText(QCoreApplication.translate("MainWindow", u"DN", None))
-        self.btnLeft.setText(QCoreApplication.translate("MainWindow", u"LT", None))
-        self.btnForward.setText(QCoreApplication.translate("MainWindow", u"FD", None))
-        self.btnRight.setText(QCoreApplication.translate("MainWindow", u"RT", None))
-        self.btnBackward.setText(QCoreApplication.translate("MainWindow", u"BD", None))
-        self.lblHeightAngle.setText(QCoreApplication.translate(
-            "MainWindow", u"Angle: 360 degrees", None))
+        self.btnLeft.setText(QCoreApplication.translate("MainWindow", u"<", None))
+        self.btnForward.setText(QCoreApplication.translate("MainWindow", u"^", None))
+        self.btnRight.setText(QCoreApplication.translate("MainWindow", u">", None))
+        self.btnBackward.setText(QCoreApplication.translate("MainWindow", u"v", None))
+        self.lblHeightAngle.setText(QCoreApplication.translate("MainWindow", u"Angle:", None))
+        self.lblInfo.setText(QCoreApplication.translate("MainWindow", u"Info", None))
+        self.chkXYRestricted.setText(QCoreApplication.translate(
+            "MainWindow", u"XY restricted", None))
+        self.btnRotLeft.setText(QCoreApplication.translate("MainWindow", u"<", None))
+        self.btnRotUp.setText(QCoreApplication.translate("MainWindow", u"^", None))
+        self.btnRotRight.setText(QCoreApplication.translate("MainWindow", u">", None))
+        self.btnRotDown.setText(QCoreApplication.translate("MainWindow", u"v", None))
+        self.lbl2.setText(QCoreApplication.translate("MainWindow", u"Rotation:", None))
+        self.lbl1.setText(QCoreApplication.translate("MainWindow", u"Movement:", None))
         self.tabMain.setTabText(self.tabMain.indexOf(self.tab),
                                 QCoreApplication.translate("MainWindow", u"Camera", None))
+        self.lbl3.setText(QCoreApplication.translate("MainWindow", u"Keyboard keys:\n"
+                                                     "\n"
+                                                     "W: Move forward\n"
+                                                     "S: Move backward\n"
+                                                     "A: Move left\n"
+                                                     "D: Move right\n"
+                                                     "E: Move up\n"
+                                                     "Q: Move down\n"
+                                                     "\n"
+                                                     "Arrow-L: Rot left\n"
+                                                     "Arrow-R: Rot right\n"
+                                                     "Arrow-U: Rot up\n"
+                                                     "Arrow-D: Rot down", None))
+        self.tabMain.setTabText(self.tabMain.indexOf(self.tabHelp),
+                                QCoreApplication.translate("MainWindow", u"Help", None))
 # ===========================================================================
 macroWindow = MacroWindow()
 # ===========================================================================
